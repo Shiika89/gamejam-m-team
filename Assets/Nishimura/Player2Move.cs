ecbd92321e282582d99f6949da01a66c9599b201
LRcatR <v_t3ixipmi@outlook.jp>
2021-08-07 15:30:38 +0900

    PlayerMoveの追加

---
+++ Assets/Nishimura/Player2Move.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using UnityEngine;
+
+public class Player2Move : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] float speed;
+    private Vector2 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        if ((x != 0) || (z != 0))
+        {
+            Debug.Log("stick:" + x + "," + z);
+        }
+
+        pos += new Vector2(x * speed, z * speed);
+        transform.position = pos;
+
+        if (Input.GetButtonDown("Player2Horizontal"))
+        {
+            Debug.Log("押された");
+        }
+    }
+}
