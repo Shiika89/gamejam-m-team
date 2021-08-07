5b9e7c9a1e32d00ada16c1d83fdbbbaf9e5e83b9
shiika89 <kazuki_plant@yahoo.co.jp>
2021-08-07 18:57:30 +0900

    ボムから弾が発射されるように変更

---
+++ Assets/Ishii/CreateBullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBullet : MonoBehaviour
+{
+    [SerializeField] GameObject m_bullet;
+    [SerializeField] GameObject m_gb;
+    [SerializeField] float m_bulletSpeed = 1;
+    [SerializeField] float m_interval;
+    float m_timar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_timar += Time.deltaTime;
+        if (m_timar > m_interval)
+        {
+            if (m_bullet)
+            {
+                var bullet = Instantiate(m_bullet, transform.position, Quaternion.identity);
+                Vector3 shotForward = Vector3.Scale((transform.position - m_gb.transform.position), new Vector3(1, 1, 0)).normalized;
+                bullet.GetComponent<Rigidbody2D>().velocity = shotForward * m_bulletSpeed;
+            }
+
+            m_timar = 0;
+        }
+    }
+}
---
+++ Assets/Ishii/Player1Direction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Direction : MonoBehaviour
+{
+    [SerializeField] float m_directionSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal2");
+        float z = Input.GetAxis("Vertical2");
+
+        if ((x != 0) || (z != 0))
+        {
+            var direction = new Vector3(0, 0, x + z);
+            transform.localRotation = Quaternion.LookRotation(direction);
+        }
+    }
+}
