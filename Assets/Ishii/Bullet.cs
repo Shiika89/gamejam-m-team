b2ac6ecfb957473cd54753fd882b8730b2fda5bd
shiika89 <kazuki_plant@yahoo.co.jp>
2021-08-07 15:31:37 +0900

    オブジェクトの方向に飛ぶ弾のscriptを作成

---
+++ Assets/Ishii/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float m_bulletSpeed = 1;
+    [SerializeField] GameObject m_gb;
+
+    private Rigidbody2D m_rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+        Vector3 h = m_gb.transform.up.normalized * m_bulletSpeed;
+        m_rb.velocity = h;
+    }
+}
