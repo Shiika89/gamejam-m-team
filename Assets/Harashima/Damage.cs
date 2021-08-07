eb85619d95597e3efaa32065d7151e3e1d25a449
DESKTOP-SNKM51H\vantan <harashima318@gmail.com>
2021-08-07 14:47:18 +0900

    弾がプレイヤーに当たった時のダメージスクリプトを追加

---
+++ Assets/Harashima/Damage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    //hpを他のスクリプトから持ってくる
+    int hp_jikken= 5;
+    //ダメージいじれるようにする
+    [SerializeField] int damage_jikken = 1;
+    
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "bullet")
+        {
+            //playerのHPからダメージ文を引く
+            hp_jikken -= damage_jikken;
+            Debug.Log(hp_jikken);
+        }
+    }
+}
