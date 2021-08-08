66963140679e5dc6bfd9d88af810aedcc8d89b87
DESKTOP-SNKM51H\vantan <harashima318@gmail.com>
2021-08-08 13:29:44 +0900

    修正：タグの変更、ダメージスクリプトに爆弾ダメージを追加。枠外に行くと消えるスクリプトの追加。

---
+++ Assets/Harashima/Scripts/Damage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    //hpを他のスクリプトから持ってくる
+    int hp= 10;
+    //ダメージいじれるようにする
+    [SerializeField] int damage = 1;
+    [SerializeField] int bom_damage = 1;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {   
+            //playerのHPからダメージ文を引く
+            hp -= damage;
+            Debug.Log("HP="+hp);
+        }
+
+        if (collision.gameObject.tag == "BombBullet")
+        {
+            //playerのHPからダメージ文を引く
+            hp -= bom_damage;
+            Debug.Log("HP=" + hp);
+        }
+    }
+}
---
+++ Assets/Harashima/Scripts/testBombInterval.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class testBombInterval : MonoBehaviour
+{
+    [SerializeField]GameObject bomb ;
+    [SerializeField] GameObject[] Respown;
+    [SerializeField] float start_interval = 5;
+    float timer;
+
+    void Start()
+    {
+        int m =Random.Range(0,9);
+         Respown[m] = Instantiate(bomb);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        timer += Time.deltaTime;
+        if (timer>start_interval)
+        {
+            timer = 0;
+            start_interval -= 0.5f;
+
+        }
+    }
+}
