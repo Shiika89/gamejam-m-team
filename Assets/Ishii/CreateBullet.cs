b2bb310353b2a9d2983c30f0128772fbbbb174d3
shiika89 <kazuki_plant@yahoo.co.jp>
2021-08-08 13:24:00 +0900

    Revert "プレイヤーの動きと弾の発射、背景の導入"
    This reverts commit a574cf84fd46ec1133e291177b14c1d0b060cfbd.

--- Assets/Ishii/CreateBullet.cs
+++ Assets/Ishii/CreateBullet.cs
@@ -8,32 +8,28 @@ public class CreateBullet : MonoBehaviour
     [SerializeField] GameObject m_gb;
     [SerializeField] float m_bulletSpeed = 1;
     [SerializeField] float m_interval;
-    PlayerMove m_move;
     float m_timar;
 
     // Start is called before the first frame update
     void Start()
     {
-        m_move = m_gb.GetComponent<PlayerMove>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (m_move.m_fire)
+        m_timar += Time.deltaTime;
+        if (m_timar > m_interval)
         {
-            m_timar += Time.deltaTime;
-            if (m_timar > m_interval)
+            if (m_bullet)
             {
-                if (m_bullet)
-                {
-                    var bullet = Instantiate(m_bullet, transform.position, Quaternion.identity);
-                    Vector3 shotForward = Vector3.Scale((transform.position - m_gb.transform.position), new Vector3(1, 1, 0)).normalized;
-                    bullet.GetComponent<Rigidbody2D>().velocity = shotForward * m_bulletSpeed;
-                }
-
-                m_timar = 0;
+                var bullet = Instantiate(m_bullet, transform.position, Quaternion.identity);
+                Vector3 shotForward = Vector3.Scale((transform.position - m_gb.transform.position), new Vector3(1, 1, 0)).normalized;
+                bullet.GetComponent<Rigidbody2D>().velocity = shotForward * m_bulletSpeed;
             }
+
+            m_timar = 0;
         }
     }
 }
