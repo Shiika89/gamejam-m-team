c046886c418af2b7fce939dcf1abebf3e004a2df
LRcatR <v_t3ixipmi@outlook.jp>
2021-08-08 13:35:57 +0900

    爆弾生成のスクリプトの追加

---
+++ Assets/Nishimura/Player1.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player1 : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] float speed;
+    private Vector2 pos;
+    private int padIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var allGamePads = Gamepad.all;
+
+        Vector2 leftInput = allGamePads[padIndex].leftStick.ReadValue();
+
+        pos += leftInput;
+        this.transform.position = pos;
+        //float x = Input.GetAxis("Horizontal3");
+        //float z = Input.GetAxis("Vertical3");
+
+        //if ((x != 0) || (z != 0))
+        //{
+        //    Debug.Log("Con:2 Lstick:" + x + "," + z);
+        //}
+
+        //pos += new Vector2(x * speed, z * speed);
+        //this.transform.position = pos;
+
+        //float x2 = Input.GetAxis("Horizontal4");
+        //float z2 = Input.GetAxis("Vertical4");
+
+        //if ((x2 != 0) || (z2 != 0))
+        //{
+        //    Debug.Log("Con:2 Rstick:" + x2 + "," + z2);
+        //}
+    }
+}
---
+++ Assets/Nishimura/Test.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] GameObject[] re;
+    [SerializeField] GameObject[] tagObject;
+    [SerializeField] GameObject bom;
+
+    Transform pos;
+    [SerializeField] float time;
+    [SerializeField] float timeInterval = 5f;
+    [SerializeField] float timeDecrease = 0.5f;
+    [SerializeField] float lastTime = 2;
+    [SerializeField] int numToGenerate = 1;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+        Instantiate(re[Random.Range(0, re.Length)]);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Check("Finish");
+
+        time += Time.deltaTime;
+        if (timeInterval == lastTime && time > timeInterval)
+        {
+            time = 0;
+            if (tagObject.Length < 100) BomGenerater(numToGenerate);
+        }
+        else if (time > timeInterval)
+        {
+            time = 0;
+            timeInterval -= timeDecrease;
+            if (tagObject.Length < 100) BomGenerater(numToGenerate);
+        }
+
+
+    }
+
+
+    void BomGenerater(int i)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            pos = re[Random.Range(0, re.Length)].transform;
+            GameObject a = Instantiate(bom, pos);
+        }
+        //tagObject = GameObject.FindGameObjectsWithTag("Bom");
+    }
+
+    void Check(string tagName)
+    {
+        tagObject = GameObject.FindGameObjectsWithTag(tagName);
+    }
+
+}
