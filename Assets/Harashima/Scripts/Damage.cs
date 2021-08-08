292924bd23cebe9b0e884e1590fa6e3c25c76437
shiika89 <kazuki_plant@yahoo.co.jp>
2021-08-08 19:26:29 +0900

    コミット

---
+++ Assets/Harashima/Scripts/Damage.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    //hpを他のスクリプトから持ってくる
+    [SerializeField] static public int hp = 30;
+    //ダメージいじれるようにする
+    [SerializeField] int damage = 1;
+    [SerializeField] int bom_damage = 1;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            //playerのHPからダメージ文を引く
+            hp -= damage;
+            Destroy(collision.gameObject);
+            Debug.Log(hp);
+            if (hp <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        if (collision.gameObject.tag == "BombBullet")
+        {
+            //playerのHPからダメージ文を引く
+            hp -= bom_damage;
+            Destroy(collision.gameObject);
+            //Debug.Log(hp);
+            if (hp <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
