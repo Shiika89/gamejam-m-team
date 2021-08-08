e64c46f61286e5e2c7681e24a857ae8a6c97ef54
DESKTOP-EL957NV\vantan <obaru.namu1101@gmail.com>
2021-08-08 13:33:42 +0900

    キャラのHPが０になった際に画像が削除され勝利の文字が出る

---
+++ Assets/Obaru/P1win.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class P1win : MonoBehaviour
+{
+
+    [SerializeField]
+    private float player2_hp = 10;
+
+    [SerializeField]
+    GameObject bakuhatu = null;
+
+    [SerializeField]
+    GameObject p1_win = null;
+
+
+    void Update()
+    {
+        if (player2_hp <= 0)
+        {
+            if (bakuhatu)
+            {
+                Instantiate(bakuhatu, this.transform.position, bakuhatu.transform.rotation);
+            }
+            if (p1_win)
+            {
+                Instantiate(p1_win, p1_win.transform.position, p1_win.transform.rotation);
+            }
+            Destroy(this.gameObject);
+            
+        }
+
+    }
+}
---
+++ Assets/Obaru/P2win.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class P2win : MonoBehaviour
+{
+    [SerializeField]
+    private float player1_hp = 10;
+   
+    [SerializeField]
+    GameObject bakuhatu = null;
+
+    [SerializeField]
+    GameObject p2_win = null;
+
+    void Update()
+    {
+        if (player1_hp <= 0)
+        {
+            if (bakuhatu)
+            {
+                Instantiate(bakuhatu, this.transform.position, bakuhatu.transform.rotation);
+            }
+            if (p2_win)
+            {
+                Instantiate(p2_win, p2_win.transform.position, p2_win.transform.rotation);
+            }
+            Destroy(this.gameObject);
+
+        }
+
+    }
+}
